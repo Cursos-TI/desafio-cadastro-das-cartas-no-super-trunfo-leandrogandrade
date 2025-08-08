89b5e32a62eac0470a4208019ba30eca20216a87
leandrogandrade <leandro@adecores.com.br>
2025-08-08 03:38:32 +0000

    Correção das variáveis iguais e aumento de caractere na variável char

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,7 +19,7 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
         char estado1, estado2;
-        char codigocarta1[3], codigocarta2[3];
+        char codigocarta1[4], codigocarta2[4];
         char nomecidade1[20], nomecidade2[20];
         int populacao1, populacao2;
         float area1, area2;
@@ -84,13 +84,13 @@ int main() {
     //Exibe os dados coletados da Carta 02
     printf("* Exibindo os dados da Carta 02 *\n");
     printf(" \n");
-    printf("Estado: %c\n",estado1);
-    printf("Código da Carta: %s\n", codigocarta1);
-    printf("Nome da Cidade: %s\n", nomecidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área Total: %.2f Km²\n", area1);
-    printf("PIB: R$ %.2f\n", pib1);
-    printf("Números de Pontos Turísticos: %d\n", pt1);
+    printf("Estado: %c\n",estado2);
+    printf("Código da Carta: %s\n", codigocarta2);
+    printf("Nome da Cidade: %s\n", nomecidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área Total: %.2f Km²\n", area2);
+    printf("PIB: R$ %.2f\n", pib2);
+    printf("Números de Pontos Turísticos: %d\n", pt2);
     printf(" \n");
     printf("Resultados exibidos com sucesso!");
     printf(" \n");
