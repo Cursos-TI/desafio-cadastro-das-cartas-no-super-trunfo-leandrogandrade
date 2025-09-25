598b205dd8aa0dc00609a65e5760a79282bafa5d
Leandro <leandro@adecores.com.br>
2025-09-24 22:25:03 -0300

    Atualização do condigo para o nível aventureiro do tema 3

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países - Tema 3 - Novato
+// Desafio Super Trunfo - Países - Tema 3 - Aventureiro
 
 int main() {
 
       
-     /* 
+/*
         //Para teste de funcionalidade do programa
         char estado1[3]= "RJ", estado2[3] = "RJ";
         char codigocarta1[4] = "A01", codigocarta2[4] = "B01";
@@ -13,13 +13,13 @@ int main() {
         unsigned long int populacao1 = 170000, populacao2 = 150000;
         float area1 = 813.42, area2 = 924.30;
         float pib1 = 2.60, pib2 = 1.95;
-        int pontosturisticos1 = 365, pontosturisticos2 = 300;
+        int pontosturisticos1 = 365, pontosturisticos2 = 300, escolhadojogador;
         float dpop1, dpop2;
         float pibcap1, pibcap2;
         float SuperPoder1;
         float SuperPoder2;
         float resultadoPopulacao, resultadoArea, resultadoPIB, resultadoTuristicos, resultadoDensidade, resultadoPIBperCapita, resultadoSuperPoder;
-    */
+ */
 
         char estado1[3], estado2[3];
         char codigocarta1[4], codigocarta2[4];
@@ -27,7 +27,7 @@ int main() {
         unsigned long int populacao1, populacao2;
         float area1, area2;
         float pib1, pib2;
-        int pontosturisticos1, pontosturisticos2;
+        int pontosturisticos1, pontosturisticos2, escolhadojogador;
         float dpop1, dpop2;
         float pibcap1, pibcap2;
         float SuperPoder1;
@@ -36,7 +36,7 @@ int main() {
     
     //Título do Programa
     printf(" ------------------------------------------------\n");
-    printf("| ** Bem vindo ao Desafio Super Trunfo - v4.0 ** |\n");
+    printf("| ** Bem vindo ao Desafio Super Trunfo - v5.0 ** |\n");
     printf(" ------------------------------------------------\n");
     printf(" \n");
 
@@ -88,7 +88,7 @@ int main() {
     printf(" \n");
     printf("----------------------------------\n");
     printf(" \n");
-    
+   
     //
     //Exibe os dados coletados da Carta 01
     //
@@ -140,15 +140,86 @@ int main() {
     printf(" \n");
     
     // Exibição dos resultados das comparações    
-    printf("Comparação das Cartas (Atributo: Super Poder):\n");
-    printf("Carta 01 - %s (%s): Super Poder: %.2f\n", nomecidade1, estado1, SuperPoder1);
-    printf("Carta 02 - %s (%s): Super Poder: %.2f\n", nomecidade2, estado2, SuperPoder2);
+ 
 
-    // Compara as cartas
-    if (SuperPoder1 > SuperPoder2) {
-        printf("Carta 01 - %s (%s) venceu com Super Poder: %.2f\n", nomecidade1, estado1, SuperPoder1);
-    } else {
-        printf("Carta 02 - %s (%s) venceu com Super Poder: %.2f\n", nomecidade2, estado2, SuperPoder2);
+    //Menu Interativo para escolha de um atributo para comparação
+    printf("* Comparação das Cartas - Escolha um atributo *\n");
+    printf(" \n");
+    printf("1. População\n");
+    printf("2. Área Total\n");
+    printf("3. PIB\n");
+    printf("4. Números de Pontos Turísticos\n");
+    printf("5. Densidade Populacional\n");
+    printf("6. Super Poder\n");
+    scanf("%d", &escolhadojogador);
+    printf(" \n");
+
+    // Exibi o resultado da caparação da carta escolhida 
+    switch (escolhadojogador)
+    {
+    case 1:
+        printf("Você escolheu População.\n");
+        if (populacao1 > populacao2) {
+            printf("Carta 01 - %s (%s) venceu com População: %lu\n", nomecidade1, estado1, populacao1);
+        } else if (populacao1 < populacao2) {
+            printf("Carta 02 - %s (%s) venceu com População: %lu\n", nomecidade2, estado2, populacao2);
+        } else {
+           printf("Empate!\n");
+        }
+        break;
+     case 2:
+        printf("Você escolheu Área Total.\n");
+        if (area1 > area2) {
+            printf("Carta 01 - %s (%s) venceu com Área Total: %.2f\n", nomecidade1, estado1, area1);
+        } else if (area1 < area2) {
+            printf("Carta 02 - %s (%s) venceu com Área Total: %.2f\n", nomecidade2, estado2, area2);
+        } else {
+           printf("Empate!\n");
+        }
+        break;       
+      case 3:
+        printf("Você escolheu PIB.\n");
+        if (pib1 > pib2) {
+            printf("Carta 01 - %s (%s) venceu com PIB: %.2f\n", nomecidade1, estado1, pib1);
+        } else if (pib1 < pib2) {
+            printf("Carta 02 - %s (%s) venceu com PIB: %.2f\n", nomecidade2, estado2, pib2);
+        } else {
+           printf("Empate!\n");
+        }
+        break;
+     case 4:
+        printf("Você escolheu Números de Pontos Turísticos.\n");
+        if (pontosturisticos1 > pontosturisticos2) {
+            printf("Carta 01 - %s (%s) venceu com Números de Pontos Turísticos: %d\n", nomecidade1, estado1, pontosturisticos1);
+        } else if (pontosturisticos1 < pontosturisticos2) {
+            printf("Carta 02 - %s (%s) venceu com Números de Pontos Turísticos: %d\n", nomecidade2, estado2, pontosturisticos2);
+        } else {
+           printf("Empate!\n");
+        }
+        break;
+    case 5:
+        printf("Você escolheu Densidade Populacional.\n");
+        if (dpop1 < dpop2) {
+            printf("Carta 01 - %s (%s) venceu com Densidade Populacional: %.2f\n", nomecidade1, estado1, dpop1);
+        } else if (dpop1 > dpop2) {
+            printf("Carta 02 - %s (%s) venceu com Densidade Populacional: %.2f\n", nomecidade2, estado2, dpop2);
+        } else {
+           printf("Empate!\n");
+        }
+        break;
+     case 6:
+        printf("Você escolheu Super Poder.\n");
+        if (populacao1 > populacao2) {
+            printf("Carta 01 - %s (%s) venceu com Super Poder: %.2f\n", nomecidade1, estado1, SuperPoder1);
+        } else if (populacao1 < populacao2) {
+            printf("Carta 02 - %s (%s) venceu com Super Poder: %.2f\n", nomecidade2, estado2, SuperPoder2);
+        } else {
+           printf("Empate!\n");
+        }
+        break;
+    default:
+        printf("Opção inválida.\n");
+        break;
     }
 
     //
@@ -157,8 +228,6 @@ int main() {
     printf(" \n");
     printf("Resultados exibidos com sucesso!");
     printf(" \n");
-    printf("Pressione qualquer tecla para sair...\n");
-    scanf(" %c"); // Espera por uma tecla para encerrar o programa
-
+  
     return 0;
 }
